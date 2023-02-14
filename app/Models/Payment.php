9482ae5722b14b5dd4cbc36fd9a3d029d96c82e2
Sitt Aung <sittaung07@gmail.com>
2023-02-14 13:00:14 +0630

    Create payments

---
+++ app/Models/Payment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['product_id', 'user_id', 'voucher_id', 'subtotal', 'taxes', 'total'];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             ProductSeeder::class,
             VoucherSeeder::class,
+            PaymentSeeder::class,
         ]);
     }
 }
