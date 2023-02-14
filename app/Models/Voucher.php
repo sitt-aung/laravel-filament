a3b45c02f02166ee6943059ec85be624bff1201c
Sitt Aung <sittaung07@gmail.com>
2023-02-14 13:01:06 +0630

    HasMany relationship - use withCount in table

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Voucher extends Model
 {
@@ -15,5 +16,10 @@ class Voucher extends Model
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
     }
 }
