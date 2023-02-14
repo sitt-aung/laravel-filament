f973825c5479c1d889e21757b2ac67a6f2143a16
Sitt Aung <sittaung07@gmail.com>
2023-02-14 14:10:23 +0630

    Change password action in the edit page and in the table

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -5,14 +5,19 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
+use Filament\Facades\Filament;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class UserResource extends Resource
 {
@@ -24,7 +29,8 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('email')->email()->required(),
             ]);
     }
 
@@ -45,6 +51,28 @@ class UserResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Action::make('changePassword')
+                    ->form([
+                        TextInput::make('new_password')
+                            ->password()
+                            ->label('New password')
+                            ->required()
+                            ->rule(Password::default()),
+                        TextInput::make('new_password_confirmation')
+                            ->password()
+                            ->label('Confirm new password')
+                            ->required()
+                            ->same('new_password')
+                            ->rule(Password::default()),
+                    ])
+                    ->action(function (User $record, array $data) {
+                        $record->update([
+                            'password' => Hash::make($data['new_password'])
+                        ]);
+
+                        Filament::notify('success', 'Password updated successfully');
+                    })
+                    ->icon('heroicon-s-key'),
             ])
             ->bulkActions([
                 //
